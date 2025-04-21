bbb2c18a1c9689038895313dc8ff47bb1808d682
Apoorv soni <apoorv.soni@coditas.com>
2025-04-21 20:33:21 +0530

    final commit

--- lib/feature/user_profile/presentation/bloc/user_profile_state.dart
+++ lib/feature/user_profile/presentation/bloc/user_profile_state.dart
@@ -36,14 +36,6 @@ class UserProfileEditState extends UserProfileState {
   @override
   List<Object> get props => <Object>[isEditing];
 }
-class UserProfileEditState extends UserProfileState {
-  final bool isEditing;
-
-  UserProfileEditState(this.isEditing);
-
-  @override
-  List<Object> get props => [isEditing];
-}
 
 class UserProfileError extends UserProfileState {
 
